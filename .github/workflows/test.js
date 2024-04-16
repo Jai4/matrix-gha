10595621315039c9b910a4003535117c9328dd5f
Jai Gowtham <jaigowtham147@gmail.com>
2024-04-16 16:54:30 +0530

    Update test.js

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -9,29 +9,31 @@ var matrix  = []
 if (args[1]=="us") {
   matrix = [
     {
-    region:"us-east-1",
-    sessionname: "GithubActionsDeploySlsToProdUsFromCdPipeline",
+    "region":"us-east-1",
+    "sessionname": "GithubActionsDeploySlsToProdUsFromCdPipeline",
     }
   ]
 } else if (args[1]=="au") {
   matrix = [
     {
-    region:"ap-southeast-2",
-    sessionname: "GithubActionsDeploySlsToProdAuFromCdPipeline",
+    "region":"ap-southeast-2",
+    "sessionname": "GithubActionsDeploySlsToProdAuFromCdPipeline",
     }
   ]
 } else {
  matrix = [
     {
-    region:"us-east-1",
-    sessionname: "GithubActionsDeploySlsToProdUsFromCdPipeline",
+    "region":"us-east-1",
+    "sessionname": "GithubActionsDeploySlsToProdUsFromCdPipeline",
     },
     {
-    region:"ap-southeast-2",
-    sessionname: "GithubActionsDeploySlsToProdAuFromCdPipeline",
+    "region":"ap-southeast-2",
+    "sessionname": "GithubActionsDeploySlsToProdAuFromCdPipeline",
     }
   ]
 }
+
+console.log(JSON.stringify(matrix))
 
 core.setOutput('matrix', JSON.stringify(matrix))
 
