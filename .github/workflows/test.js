3877e8f9e9cc1e628ae8c3684df5a756d246a078
Jai Gowtham <jaigowtham147@gmail.com>
2024-04-16 16:43:33 +0530

    Update test.js

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -4,3 +4,35 @@ var args = process.argv.slice(2);
 
 console.log(args[0]);
 console.log(args[1]);
+
+var matrix  = []
+if (args[1]=="us") {
+  matrix = [
+    {
+    region:"us-east-1",
+    sessionname: "GithubActionsDeploySlsToProdUsFromCdPipeline",
+    }
+  ]
+} else if (args[1]=="au") {
+  matrix = [
+    {
+    region:"ap-southeast-2",
+    sessionname: "GithubActionsDeploySlsToProdAuFromCdPipeline",
+    }
+  ]
+} else {
+ matrix = [
+    {
+    region:"us-east-1",
+    sessionname: "GithubActionsDeploySlsToProdUsFromCdPipeline",
+    },
+    {
+    region:"ap-southeast-2",
+    sessionname: "GithubActionsDeploySlsToProdAuFromCdPipeline",
+    }
+  ]
+}
+
+core.setOutput('matrix', JSON.stringify(matrix))
+
+ 
