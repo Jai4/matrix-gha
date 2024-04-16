7ba9766197dd35796b8e44cdf1963f1365b97deb
Jai Gowtham <jaigowtham147@gmail.com>
2024-04-16 16:30:05 +0530

    Update test.js

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,4 +1,6 @@
 // javascript
 const core = require('@actions/core');
-const myInput = core.getInput('deployment-ring', { required: true });
-console.log(myInput);
+var args = process.argv.slice(2);
+
+console.log(args[0);
+console.log(args[1);
