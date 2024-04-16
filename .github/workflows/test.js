546d57c9a7e38a32b027eb284c33aa96697d967b
Jai Gowtham <jaigowtham147@gmail.com>
2024-04-16 17:30:58 +0530

    Update test.js

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,40 +1,16 @@
 // javascript
 const core = require('@actions/core');
-var args = process.argv.slice(2);
+var args = process.argv;
 
-console.log(args[0]);
-console.log(args[1]);
+console.log(args[2])
 
-var matrix  = []
-if (args[1]=="us") {
-  matrix = [
-    {
-    "region":"us-east-1",
-    "sessionname": "GithubActionsDeploySlsToProdUsFromCdPipeline",
-    }
-  ]
-} else if (args[1]=="au") {
-  matrix = [
-    {
-    "region":"ap-southeast-2",
-    "sessionname": "GithubActionsDeploySlsToProdAuFromCdPipeline",
-    }
-  ]
-} else {
- matrix = [
-    {
-    "region":"us-east-1",
-    "sessionname": "GithubActionsDeploySlsToProdUsFromCdPipeline",
-    },
-    {
-    "region":"ap-southeast-2",
-    "sessionname": "GithubActionsDeploySlsToProdAuFromCdPipeline",
-    }
-  ]
-}
+var matrix  = {"include":[{"region":"us-east-1","sessionname": "GithubActionsDeploySlsToProdUsFromCdPipeline"}, {"region":"ap-southeast-2","sessionname": "GithubActionsDeploySlsToProdAuFromCdPipeline"}]}
+if (args[2=="us") {
+  matrix  = {"include":[{"region":"us-east-1","sessionname": "GithubActionsDeploySlsToProdUsFromCdPipeline"}]}
+} else if (args[2=="au") {
+  matrix  = {"include":[{"region":"ap-southeast-2","sessionname": "GithubActionsDeploySlsToProdAuFromCdPipeline"}]}
+} 
 
 console.log(JSON.stringify(matrix))
 
 core.setOutput('matrix', JSON.stringify(matrix))
-
- 
