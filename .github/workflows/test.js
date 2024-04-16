a0cf339106670061f8035efece4392f0dad7cce3
Jai Gowtham <jaigowtham147@gmail.com>
2024-04-16 17:32:03 +0530

    Update test.js

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -5,9 +5,9 @@ var args = process.argv;
 console.log(args[2])
 
 var matrix  = {"include":[{"region":"us-east-1","sessionname": "GithubActionsDeploySlsToProdUsFromCdPipeline"}, {"region":"ap-southeast-2","sessionname": "GithubActionsDeploySlsToProdAuFromCdPipeline"}]}
-if (args[2=="us") {
+if (args[2]=="us") {
   matrix  = {"include":[{"region":"us-east-1","sessionname": "GithubActionsDeploySlsToProdUsFromCdPipeline"}]}
-} else if (args[2=="au") {
+} else if (args[2]=="au") {
   matrix  = {"include":[{"region":"ap-southeast-2","sessionname": "GithubActionsDeploySlsToProdAuFromCdPipeline"}]}
 } 
 
